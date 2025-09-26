e2e7d2079169ade53158ac3a156c7b3c3cc24504
Tiru-99 <aayushtirmanwar1234@gmail.com>
2025-09-27 01:24:28 +0530

    add : redis pub sub added

--- backend/src/socket.ts
+++ backend/src/socket.ts
@@ -3,7 +3,9 @@ import { v4 as uuid } from "uuid";
 import { Socket } from "socket.io";
 import { ChatSocketType } from "./utils/enums";
 import { produceMessage } from "./config/kafka";
+import { pubClient , subClient } from "./config/redis";
 
+const subscribedChats = new Set<string>(); // to avoid duplicate subscriptions
 
 export const SocketIoServer = (io: Server) => {
     io.on("connection", (socket: Socket) => {
@@ -11,15 +13,23 @@ export const SocketIoServer = (io: Server) => {
             //id is chatId here 
             socket.join(id);
             console.log("Someone joined the room");
+
+            if(!subscribedChats.has(id)){
+              subClient.subscribe(id , (message : string) => {
+                const parsed = JSON.parse(message);
+                io.to(id).emit("new-message" , parsed)
+              })
+              subscribedChats.add(id);
+              console.log("Subscribed to the redis channel" + id);
+            }
         });
 
         socket.on(ChatSocketType.SEND_MESSAGE, (messageData: any) => {
             const { chatId } = messageData;
             console.log("the chat id is", chatId);
             //produce in the kafka queue
-
             produceMessage(JSON.stringify(messageData));
-            io.to(chatId).emit("new-message", messageData);
+            pubClient.publish(chatId , JSON.stringify(messageData));
         })
     })
 }
