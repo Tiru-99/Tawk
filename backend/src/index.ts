1ed90a64217d99beab844a2276c1608ee5e40c27
Tiru-99 <aayushtirmanwar1234@gmail.com>
2025-10-01 23:54:38 +0530

    fix : sequential changes in index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,7 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv'
 import compression from 'compression';
 import cookieParser from 'cookie-parser';
-import {createServer} from 'http'; 
+import { createServer } from 'http';
 import { Server } from 'socket.io';
 import cors from 'cors'
 import { types as mediasoupTypes } from 'mediasoup';
@@ -12,54 +12,55 @@ import { SocketIoServer } from './socket';
 import { SocketService } from './mediasoup/SocketServer';
 
 dotenv.config({
-    path:'./.env'
+  path: './.env'
 })
 
 
 
 const app = express();
-const server = createServer(app) ; 
+const server = createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: process.env.FRONTEND_URL ,
-    methods : ["GET" , "POST"],
-    credentials : true
+    origin: process.env.FRONTEND_URL,
+    methods: ["GET", "POST"],
+    credentials: true
   },
 });
+
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL, // Allow only this origin
+    methods: ["GET", "POST", "PUT", "DELETE"], // Allowed HTTP methods
+    allowedHeaders: ["Content-Type", "Authorization"], // Allowed headers
+    credentials: true, // Allow cookies if needed
+  })
+);
+
 
 SocketIoServer(io)
 new SocketService(io)
 // setUpMediaSoupServer(io);
 
 
-app.use(express.json({limit: "16kb"}))
-app.use(express.urlencoded({extended: true, limit: "16kb"}))
+app.use(express.json({ limit: "16kb" }))
+app.use(express.urlencoded({ extended: true, limit: "16kb" }))
 app.use(express.static("public"))
 //to compress api requests
 app.use(compression());
-app.use(
-    cors({
-      origin: process.env.FRONTEND_URL, // Allow only this origin
-      methods: ["GET", "POST", "PUT", "DELETE"], // Allowed HTTP methods
-      allowedHeaders: ["Content-Type", "Authorization"], // Allowed headers
-      credentials: true, // Allow cookies if needed
-    })
-  );
-
 app.use(cookieParser())
 
-const PORT = 5000 ; 
+const PORT = 5000;
 
 //Connect redis on startup
 (async () => {
-  await connectRedis(); 
+  await connectRedis();
 })();
 
 //consume kafka streams
-startMessageConsumer(); 
+startMessageConsumer();
 
-app.get('/' , (req, res)=>{
-    res.send("hello world this is aayush tirmanwar");
+app.get('/', (req, res) => {
+  res.send("hello world this is aayush tirmanwar");
 })
 
 
@@ -70,11 +71,11 @@ import messageRoute from './routes/message.routes';
 import imageUploadRoute from './routes/imageUpload.routes';
 
 
-app.use("/api/v1/user" , authRoute);
-app.use("/api/v1/chat" , chatRoute );
-app.use("/api/v1/message" , messageRoute);
-app.use("/api/v1/upload" , imageUploadRoute);
+app.use("/api/v1/user", authRoute);
+app.use("/api/v1/chat", chatRoute);
+app.use("/api/v1/message", messageRoute);
+app.use("/api/v1/upload", imageUploadRoute);
 
-server.listen(PORT , ()=>{
-    console.log(`The server is up and running on PORT ${PORT}`);
+server.listen(PORT, () => {
+  console.log(`The server is up and running on PORT ${PORT}`);
 })
